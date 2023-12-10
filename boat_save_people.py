3141229df4fb7d7737721456ab0e623428d8eac2
cwsong95@gmail.com <kevinsong@Kevins-MacBook-Air.local>
2023-12-10 14:45:28 -0500

    Adds boat_save_people.py to LeetCode repository
    Resolves

--- boat_save_people.py
+++ boat_save_people.py
@@ -2,12 +2,12 @@ class Solution:
 	def numRescueBoats(self, people: List[int], limit: int) -> int: 
     people.sort()
 
-    left = 0                  //two pointers method beginning
-    right = len(people) - 1   //two pointers method end   
-    boats_number = 0          //output, counter
+    left = 0                  #two pointers method beginning
+    right = len(people) - 1   #two pointers method end   
+    boats_number = 0          #output, counter
 
     while(left <= right):
-      if(left == right):      //edge case
+      if(left == right):      #edge case
         boats_number += 1
       break
 
